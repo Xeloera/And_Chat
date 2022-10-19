1b2756f3c34e4ef0c303303bc24027e49c6fecc9
Xeloera <andrewshearer98@hotmail.com>
2022-10-19 11:59:18 +0100

    gg

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
+const guessForm = document.getElementById('guess-container')
 const messageInput = document.getElementById('message-input')
+const guessInput = document.getElementById('guess-input')
 
 const name = prompt('What is your name?')
+const atitle = prompt('What is your And title?')
+const titles = []
+//titles.push(atitle)
 appendMessage(`${name} has joined`)
 socket.emit('new-user', name)
+socket.emit('new-title', atitle)
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
@@ -15,9 +21,20 @@ socket.on('user-connected', name => {
   appendMessage(`${name} connected`)
 })
 
+socket.on('new-title-added', atitle => {
+  titles.push(atitle)
+  console.log("title",atitle)
+  console.log("title2",titles)
+})
+
+
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
+
+
+
+
 
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
@@ -29,6 +46,21 @@ messageForm.addEventListener('submit', e => {
   socket.emit('send-chat-message', message)
   messageInput.value = ''
 })
+
+guessForm.addEventListener('submit', e => {
+  e.preventDefault()
+  // document.querySelector('emoji-picker')
+  // .addEventListener('emoji-click', event => appendMessage(`You: ${event}`));
+  
+  const message = guessInput.value
+  if(titles.includes(message)){
+    appendMessage("you win!")
+  }  else{
+    appendMessage("try again")
+  }
+  messageInput.value = ''
+})
+
 
 function appendMessage(message) {
   const messageElement = document.createElement('div')
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const io = require('socket.io')(3000)
 
 const users = {}
+const title = {}
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
+    console.log(name)
     socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('new-title', atitle => {
+    title[socket.id] = atitle
+    console.log('title',atitle)
+    socket.broadcast.emit('new-title-added', atitle)
   })
   socket.on('send-chat-message', message => {
     console.log(message)
@@ -15,4 +22,5 @@ io.on('connection', socket => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
+
 })
